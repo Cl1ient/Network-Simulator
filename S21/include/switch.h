e1624771996e10b4235d5564df7910e53bfebd9e
Açelya <acelya.mhrr@gmail.com>
2025-05-20 14:19:35 +0200

    modif

--- S21/include/switch.h
+++ S21/include/switch.h
@@ -1,30 +1,30 @@
 #include "adresse.h"
-#include
+#include "graphe.h"
 
 
 // entrée dans la table de commutation
-typedef struct {
+typedef struct entreeTable{
     Mac adresse;    // Adresse MAC connue
     int port;       // Port associé à l'adresse
-} EntreeTable;
+} entreeTable;
 
 // table de commu complète, utilisé dans chaque switch
-typedef struct {
-    EntreeTable entrees[TAILLE_TABLE_COMMUTATION];  // Liste des entrées (table statique)
+typedef struct tableCommutation{
+    entreeTable entrees[TAILLE_TABLE_COMMUTATION];  // Liste des entrées (table statique)
     int taille;  // Nombre actuel d’entrées valides dans la table
-} TableCommutation;
+} tableCommutation;
 
 // represente un switch d'un réseau
-typedef struct {
+typedef struct switch{
     Mac adresseMac;     // adr mac unique du switch
     int nbPort;     // nb tot de ports du switch
     int priorite;       // priorite pour le stp
-    TableCommutation tblCommu;      // tbl commu
-    Sommet* sommet;     // ref vers le sommet du graphe associé a ce switch
-} Switch
+    tableCommutation tblCommu;      // tbl commu
+    sommet* sommet;     // ref vers le sommet du graphe associé a ce switch
+} switch
 
 // créer un switch
-//Switch creationSwitch(Mac mac, int nbPort, int priorite, Sommet* sommet);
+//switch creationSwitch(Mac mac, int nbPort, int priorite, Sommet* sommet);
 
 // ajt une entrée à la table de commu
 //void ajtEntreeTable(TableCommutation* table, Mac adresse, int port);
