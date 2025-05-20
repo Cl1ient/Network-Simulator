84e02120afc9c26942d7695cac5da371122bd9dd
Açelya <acelya.mhrr@gmail.com>
2025-05-20 15:06:08 +0200

    ok

--- S21/include/adresse.h
+++ S21/include/adresse.h
@@ -11,5 +11,7 @@ typedef struct {
     unsigned char octets[4];
 } AdresseIP;
 
+char * mac_to_string(AdresseMac M, char * str_mac);
+
 void afficherIp(AdresseIp ip);
 char* ip_to_string(AdresseIP ip, char* str_ip);
--- S21/src/adresse.c
+++ S21/src/adresse.c
@@ -1,5 +1,13 @@
-#include <../include/stdio.h>
-#include "adresse.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/adresse.h"
+
+
+char * mac_to_string(AdresseMac M, char * str_mac){
+    sprintf(str_mac, "%02X:%02X:%02X:%02X:%02X:%02X",M.octets[0], M.octets[1], M.octets[2],M.octets[3], M.octets[4], M.octets[5]);
+    return str_mac; // return du pointeur pour utilisation direct
+}
 
 // afficher une adresse ip : 
 char* ip_to_string(AdresseIP ip, char* str_ip) {
