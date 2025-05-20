84aa800aaec7d558fd3988f1a6af1a2ffe5e01ac
alexis <teekzie68@gmail.com>
2025-05-20 15:07:03 +0200

    modif fonction

--- S21/main.c
+++ S21/main.c
@@ -2,7 +2,5 @@
 #include "include/adresse.h"
 
 int main(){
-    char buffer[18]; // taille min du mac sous forme string
-    AdresseMac mac = {{0x54, 0xD6, 0xA6, 0x82, 0xC5, 0x23}};
-    printf("MAC = %s\n", mac_to_string(mac, buffer));
+    
 }
--- S21/src/adresse.c
+++ S21/src/adresse.c
@@ -8,3 +8,8 @@ char * mac_to_string(AdresseMac M, char * str_mac){
     sprintf(str_mac, "%02X:%02X:%02X:%02X:%02X:%02X",M.octets[0], M.octets[1], M.octets[2],M.octets[3], M.octets[4], M.octets[5]);
     return str_mac; // return du pointeur pour utilisation direct
 }
+
+void afficher(AdresseMac mac){
+    char taille[18]; // taille min du mac sous forme string
+    printf("MAC = %s\n", mac_to_string(mac, taille));
+}
